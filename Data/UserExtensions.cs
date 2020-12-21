ae1811d763c7c1d69f73a1b24000521f2867c4cd
Oussama Souayeh <oussama.souayah@gmail.com>
2020-12-20 22:23:11 -0500

    initial commit

---
+++ Data/UserExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+
+namespace msoscarcorp.Data
+{
+    public static class UserExtensions
+    {
+        public static string FirstName(this ClaimsPrincipal user)
+            => user.FindFirst("given_name")?.Value;
+
+        public static string LastName(this ClaimsPrincipal user)
+            => user.FindFirst("family_name")?.Value;
+
+        public static string Email(this ClaimsPrincipal user)
+            => user.FindFirst("emails")?.Value;
+
+        public static bool IsApproved(this ClaimsPrincipal user){
+            bool flag;
+            if(bool.TryParse(user.FindFirst("IsApproved")?.Value, out flag)) {
+                return flag;
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace msoscarcorp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+
+                options.ProviderOptions.LoginMode = "redirect";
+            });
+
+            //other DI services
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
